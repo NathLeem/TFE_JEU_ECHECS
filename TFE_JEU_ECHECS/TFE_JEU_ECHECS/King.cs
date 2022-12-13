5cc51bea9dc3368207bfd5ed170c5b993d46c994
Leemans Nathan <200253@CCM-Asty-Moulin.local>
2022-12-13 14:36:12 +0100

    Creation

---
+++ TFE_JEU_ECHECS/TFE_JEU_ECHECS/King.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TFE_JEU_ECHECS
+{
+    class King
+    {
+        private int[,] _position = new int[8, 8];
+        private bool _dead = false;
+        private List<string> _moves = new List<string>();
+
+        public King(int[,] position, bool dead, List<string> moves)
+        {
+            this._position = position;
+            this._dead = dead;
+            this._moves = moves;
+        }
+        public void Moves()
+        {
+
+        }
+
+        public void AfficheMoves()
+        {
+
+        }
+    }
+}
---
+++ TFE_JEU_ECHECS/TFE_JEU_ECHECS/Queen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TFE_JEU_ECHECS
+{
+    class Queen
+    {
+        private int[,] _position = new int[8, 8];
+        private bool _dead = false;
+        private List<string> _moves = new List<string>();
+
+        public Queen(int[,] position, bool dead, List<string> moves)
+        {
+            this._position = position;
+            this._dead = dead;
+            this._moves = moves;
+        }
+        public void Moves()
+        {
+
+        }
+
+        public void AfficheMoves()
+        {
+
+        }
+    }
+}
